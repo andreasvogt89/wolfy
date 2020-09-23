0ca21507f540c5b5284eebbc22f876708389a28e
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-09-23 21:37:14 +0200

    added some tests for RESTful

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const app = require('./app');
 require('dotenv').config();
 const logger = require('./serverlog/logger');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+// Certificate
+const privateKey = fs.readFileSync('Z:\\dinifarb.duckdns.org\\privkey.pem', 'utf8');
+const certificate = fs.readFileSync('Z:\\dinifarb.duckdns.org\\cert.pem', 'utf8');
+const ca = fs.readFileSync('Z:\\dinifarb.duckdns.org\\chain.pem', 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+const httpPort = process.env.httpPort || 5000
+const httpsPort = process.env.httpsPort || 4556
+
+httpServer.listen(httpPort, () => {
   /* eslint-disable no-console */
-  logger.info(`Listening: http://localhost:${port}`);
+  logger.info(`Listening: http://localhost:${httpPort}`);
+  /* eslint-enable no-console */
+});
+httpsServer.listen(httpsPort, () => {
+  /* eslint-disable no-console */
+  logger.info(`Listening: http://localhost:${httpsPort}`);
   /* eslint-enable no-console */
 });
