3215398c5db23f64d06a169a534606f40e5d0b1b
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-09-24 19:33:10 +0200

    added some tests for RESTful

--- src/index.js
+++ src/index.js
@@ -4,30 +4,37 @@ const logger = require('./serverlog/logger');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-
-/*// Certificate
-const privateKey = fs.readFileSync('Z:\\dinifarb.duckdns.org\\privkey.pem', 'utf8');
-const certificate = fs.readFileSync('Z:\\dinifarb.duckdns.org\\cert.pem', 'utf8');
-const ca = fs.readFileSync('Z:\\dinifarb.duckdns.org\\chain.pem', 'utf8');
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};*/
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(app);
+const path = require('path');
 
 const httpPort = process.env.httpPort || 5544
 const httpsPort = process.env.httpsPort || 4556
+
+logger.info(fs.realpathSync('.'));
+try {
+// Certificate
+  const privateKey = fs.readFileSync(path.join(__dirname,'../cert/privkey.pem'));
+  const certificate = fs.readFileSync(path.join(__dirname,'../cert/cert.pem'));
+  const ca = fs.readFileSync(path.join(__dirname,'../cert/chain.pem'));
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+  const httpsServer = https.createServer(credentials,app);
+  httpsServer.listen(httpsPort, () => {
+    /* eslint-disable no-console */
+    logger.info(`Listening: http://localhost:${httpsPort}`);
+    /* eslint-enable no-console */
+  });
+} catch (e) {
+  logger.error('can not load certificate and start https: ' + e.message);
+}
+
+const httpServer = http.createServer(app);
 
 httpServer.listen(httpPort, () => {
   /* eslint-disable no-console */
   logger.info(`Listening: http://localhost:${httpPort}`);
   /* eslint-enable no-console */
 });
-httpsServer.listen(httpsPort, () => {
-  /* eslint-disable no-console */
-  logger.info(`Listening: http://localhost:${httpsPort}`);
-  /* eslint-enable no-console */
-});
+
