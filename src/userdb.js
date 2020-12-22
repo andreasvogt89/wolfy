2c8bd8bb231f023ce9962539ba2207e3e404bed1
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-12-22 17:35:04 +0100

    init

--- src/userdb.js
+++ src/userdb.js
@@ -1,24 +1,25 @@
-const {User} = require('./mongodb');
+const { User } = require('./mongodb');
 const bcrypt = require('bcrypt');
 const logger = require('./serverlog/logger');
 
 async function createUser() {
-try {
-  const roles = {
-    ADMIN: "Admin"
-  }
-  const user = {
-    username: "Andreas",
-    password: await bcrypt.hash("4556@A89xy$$", 10),
-    role: roles.ADMIN
-  }
-  logger.info(`Create user: ${JSON.stringify(user)}`);
-  User.create(user);
-  return "Successful created user: " + user.username;
-} catch (e){
-  return new Error(`Create user failed: ${e}`)
-}
+    try {
+        const roles = {
+            ADMIN: "Admin",
+            USER: "User"
+        }
+        const user = {
+            username: "Jugendarbeit",
+            password: await bcrypt.hash("Jala4513", 10),
+            role: roles.USER
+        }
+        logger.info(`Create user: ${JSON.stringify(user)}`);
+        User.create(user);
+        return "Successful created user: " + user.username;
+    } catch (e) {
+        return new Error(`Create user failed: ${e}`)
+    }
 }
 module.exports = {
-  createUser
+    createUser
 }
